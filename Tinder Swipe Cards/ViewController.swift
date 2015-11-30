db13ef77750ee66223a365bff0c0c445a796fe66
Zach Smoroden <zach@smoroden.com>
2015-11-29 22:44:10 -0800

    Initial Commit

---
+++ Tinder Swipe Cards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tinder Swipe Cards
+//
+//  Created by Zach Smoroden on 2015-11-29.
+//  Copyright © 2015 Zach Smoroden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
